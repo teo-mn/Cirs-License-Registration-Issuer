2560e2d03c96830ad0b29df93668e6843f84e408
Surenbayar <suugii.sd@gmail.com>
2023-11-10 16:01:30 +0800

    prepare for deployment

--- blockchain/balance.py
+++ blockchain/balance.py
@@ -4,4 +4,4 @@ from license_registration_issuer.settings import NODE_URL
 def get_balance(address: str):
     from web3 import Web3
     web3 = Web3(Web3.HTTPProvider(NODE_URL))
-    return web3.eth.get_balance(address)
+    return float(web3.from_wei(web3.eth.get_balance(web3.to_checksum_address(address)), 'ether'))
--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ python manage.py collectstatic  --noinput
 echo 'done.'
 
 echo 'Starting gunicorn'
-gunicorn --workers 1 --threads 3 --worker-connections 10 -b :1010 license_registration_issuer.wsgi
+gunicorn --workers 1 --threads 3 --worker-connections 10 -b :8000 license_registration_issuer.wsgi
